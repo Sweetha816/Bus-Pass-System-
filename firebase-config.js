874780aff588d476a6609a4f0830162f41a62200
Sweetha816 <sweethamirraa.ece2023@dce.edu.in>
2025-08-19 16:40:48 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmpnFJ4dkGG8Pakx__I4GGdoR52_PDFHI",
+  authDomain: "bus-pass-system-6ed79.firebaseapp.com",
+  projectId: "bus-pass-system-6ed79",
+  storageBucket: "bus-pass-system-6ed79.firebasestorage.app",
+  messagingSenderId: "271977650758",
+  appId: "1:271977650758:web:c81bd6c2bddfb442fa4847"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
